6c5d09abcdd570d4615c9c7be35f33476474b444
abhiucan <85837483+abhiucan@users.noreply.github.com>
2021-08-01 13:12:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,126 @@
+
+
+var wallpaper,wallpaperimg
+var astronautimg,astronaut
+var bath,bathimg
+var brush,brushimg
+var drink,drinkimg
+var eat,eatimg
+var gym,gymimg
+var move,moveimg
+var edge
+
+
+
+
+function preload() {
+
+  wallpaperimg = loadImage("iss.png")
+  astronautimg = loadImage("sleep.png")
+  bathimg = loadAnimation("bath1.png","bath2.png")
+  brushimg = loadAnimation("brush.png")
+  drinkimg = loadAnimation("drink1.png","drink2.png")
+  eatimg = loadAnimation("eat1.png","eat2.png")
+  gymimg = loadAnimation("gym1.png","gym2.png","gym11.png","gym12.png",)
+  moveimg = loadAnimation("move.png","move1.png","move2.png")
+
+
+
+
+
+
+
+}
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  
+wallpaper = createSprite(400,200)
+wallpaper.addImage("screen",wallpaperimg)
+
+astronaut = createSprite(width/2,height/2)
+astronaut.addImage("sleep",astronautimg)
+astronaut.scale=0.2
+
+
+edge = createEdgeSprites()
+
+
+
+
+}
+
+function draw() {
+  background(255,255,255); 
+
+
+  if(keyDown("UP_ARROW")){
+ astronaut.addAnimation("brushing",brushimg)
+ astronaut.changeAnimation("brushing")
+ astronaut.x=width/2
+ astronaut.y=height/2
+ astronaut.velocityX=0.0
+ astronaut.velocityY=0.0
+  }
+
+
+ if(keyDown("DOWN_ARROW")){
+  astronaut.addAnimation("gyming",gymimg)
+  astronaut.changeAnimation("gyming")
+  astronaut.x=width/2
+  astronaut.y=height/2
+  astronaut.velocityX=0.0
+ astronaut.velocityY=0.0
+ }
+
+ if(keyDown("LEFT_ARROW")){
+  astronaut.addAnimation("eating",eatimg)
+  astronaut.changeAnimation("eating")
+  astronaut.x=width/2
+  astronaut.y=height/2
+  astronaut.velocityX=0.0
+ astronaut.velocityY=0.0
+ }
+
+ if(keyDown("RIGHT_ARROW")){
+  astronaut.addAnimation("bathing",bathimg)
+  astronaut.changeAnimation("bathing")
+  astronaut.x=width/2
+  astronaut.y=height/2
+  astronaut.velocityX=0.0
+ astronaut.velocityY=0.0
+ }
+
+ if(keyDown("M")){
+astronaut.addAnimation("moveing",moveimg)
+astronaut.changeAnimation("moveing",moveimg)
+ astronaut.x=width/2
+ astronaut.y=height/2
+ astronaut.velocityX=0.5
+ astronaut.velocityY=0.5
+ astronaut.bounceOff(edge)
+ }
+
+ if(keyDown("D")){
+  astronaut.addAnimation("drinking",drinkimg)
+  astronaut.changeAnimation("drinking")
+  astronaut.x=width/2
+  astronaut.y=height/2
+  astronaut.velocityX=0.0
+ astronaut.velocityY=0.0
+
+ }
+
+
+  drawSprites();
+  }
+  
+  
+
+
+
+
+
+
+
